d03163e4ccf69fdbb8dd4afbbe7bf1bcdc2a6e3d
ClaudioF9 <claudioferreira0284@gmail.com>
2023-03-07 17:26:57 +0000

    Added questions to html.js

--- index.js
+++ index.js
@@ -13,3 +13,89 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const teamMembers = [];
+
+const managerQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the name of your team manager?",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is your team manager's employee ID?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your team manager's email address?",
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is your team manager's office number?",
+    },
+];
+
+const engineerQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the engineer's name?",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the engineer's employee ID?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the engineer's email address?",
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is the engineer's GitHub username?",
+    },
+];
+
+const internQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the intern's name?",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the intern's employee ID?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the intern's email address?",
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "Where did the intern attend school?",
+    },
+];
+
+const addEngineerQuestion = {
+    type: "list",
+    name: "addEngineer",
+    message: "would you like to add an engineer",
+    choices: ["Yes, ad an engineer", "No, finish building the team"],
+};
+
+const addInternQuestion = {
+    type: "list",
+    name: "addIntern",
+    message: "Would you like to add an intern?",
+    choices: ["Yes, add an intern", "No, finish building the team"],
+};
+
+
